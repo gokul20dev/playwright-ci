64b4fb1d762a882b34b197322901d7e99623f027
gokul <gopalakrishnan93843@gmail.com>
2025-11-03 15:55:20 +0530

    Update example.spec.ts

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,4 +1,4 @@
-import { test, expect } from '@playwright/test';
+import { testsss, expectsss } from '@playwright/test';
 
 test('Homepage has title', async ({ page }) => {
   await page.goto('https://playwright.dev/');
