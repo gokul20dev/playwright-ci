172a06651beac6d9c25ce8b0f8461ae734650ac4
gokul <gopalakrishnan93843@gmail.com>
2025-11-04 11:59:37 +0530

    Fix import statement in example.spec.ts

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,4 +1,4 @@
-import { tests, expect } from '@playwright/test';
+import { test, expect } from '@playwright/test';
 
 test('Homepage has title', async ({ page }) => {
   await page.goto('https://playwright.dev/');
