58fc94eae30e19d31b5d174f7f81e6a761251f5d
gokul <gopalakrishnan93843@gmail.com>
2025-11-06 15:04:00 +0530

    Update run_tests.sh

--- run_tests.sh
+++ run_tests.sh
@@ -3,16 +3,6 @@ set +e
 
 cd /workspace
 
-echo "▶️ Configuring Gmail SMTP..."
-cat <<EOF > /root/.muttrc
-set from="$GMAIL_USER"
-set realname="Jenkins Automation"
-set smtp_url="smtp://smtp.gmail.com:587"
-set smtp_pass="$GMAIL_PASS"
-set ssl_starttls=yes
-set ssl_force_tls=yes
-EOF
-
 echo "▶️ Install dependencies..."
 npm ci --quiet
 
@@ -24,15 +14,13 @@ echo "▶️ Zipping report..."
 zip -r playwright-report.zip playwright-report
 
 if [ $TEST_STATUS -eq 0 ]; then
-    SUBJECT="✅ Playwright Tests Passed"
-    BODY="All UI tests passed ✅\nHTML report attached."
+    export TEST_SUBJECT="✅ Playwright Tests Passed"
 else
-    SUBJECT="❌ Playwright Tests Failed"
-    BODY="Some UI tests failed ❌\nHTML report attached."
+    export TEST_SUBJECT="❌ Playwright Tests Failed"
 fi
 
-echo "▶️ Sending Email..."
-echo -e "$BODY" | mutt -s "$SUBJECT" -a playwright-report.zip -- "$GMAIL_USER"
+echo "▶️ Sending Email via Node.js..."
+node send_report.js
 
 echo "✅ Email send attempt complete!"
 exit 0
