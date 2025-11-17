2b496c863da7ebfdd2a8f8b5cbd0ceb5446edbfa
gokul <gopalakrishnan93843@gmail.com>
2025-11-17 17:30:10 +0530

    Update send_report.js

--- send_report.js
+++ send_report.js
@@ -52,7 +52,7 @@ let emailBody = `
       <a href="${reportUrl}" target="_blank"
         style="background:#1976d2; color:#fff; padding:10px 14px; border-radius:6px;
                text-decoration:none; font-weight:bold;">
-        View Full HTML Report
+        View Full HML Report
       </a>
     </p>` : ""}
 
